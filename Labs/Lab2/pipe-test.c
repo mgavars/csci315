626d4ce5f29211ee9ec85597f7e13237a12c6560
mag051 <mag051@bucknell.edu>
2019-09-10 08:18:51 -0400

    Fixed up variables

--- Labs/Lab2/pipe-test.c
+++ Labs/Lab2/pipe-test.c
@@ -10,6 +10,10 @@
 #include <unistd.h>
 #include <sys/types.h>
 
+#define BUFFER_SIZE 25
+#define READ_END 0
+#define WRITE_END 1
+
 int main(void) {
   char write_msg[BUFFER_SIZE] = "Greetings";
   char read_msg[BUFFER_SIZE];
